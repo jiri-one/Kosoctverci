3af1592f1358995da26dd51f0ce2e948c4a0a110
jiri.one <nemec@jiri.one>
2021-04-09 20:13:36 +0200

    finished kamen,nuzky,papir

--- 04/07_kamen_nuzky_papir.py
+++ 04/07_kamen_nuzky_papir.py
@@ -1,43 +1,29 @@
-from random import randrange
-cislo = randrange(3)
+# říkal jsem si, že tenhle program chce prostě zkrátit ;)
+
+from random import choice
+
+slova = ["kámen", "nůžky", "papír"]
+
+def vitez(tah_hrace, tah_pocitace):
+    volby = {tah_hrace: "hráč" , tah_pocitace: "počítač"}
+    if tah_hrace == tah_pocitace:
+        print("Remíza!")
+    if "kámen" in volby.keys() and "nůžky" in volby.keys():
+        print(f"""Kámen tupí nůžky! Vyhrává {volby["kámen"]}.""")
+    elif "nůžky" in volby.keys() and "papír" in volby.keys():
+        print(f"""Nůžky sříhají papír! Vyhrává {volby["nůžky"]}.""")
+    elif "papír" in volby.keys() and "kámen" in volby.keys():
+        print(f"""Papír zabalí kámen! Vyhrává {volby["papír"]}.""")    
+
+while tah_hrace := input("Jaký volíš tah? "):
+    tah_pocitace = choice(slova)
+    if tah_hrace in slova:
+        vitez(tah_hrace, tah_pocitace)
+    elif tah_hrace == "konec":
+        print("Chápu, tahle hra je nuda. Končíme!")
+        break
+    else:
+        print("Tenhle program zná jen: kámen, nůžky, papír a nebo konec.")
 
 
-if cislo == 0:
-    tah_pocitace = "kámen"
-   
-elif cislo == 1:
-    tah_pocitace = "nůžky"
-    
-else:
-    tah_pocitace = "papír"
-    
-#print(tah_pocitace)
 
-tah_hrace = input("Jaký volíš tah? ")
-
-if tah_hrace == "kámen":
-    if tah_pocitace == "kámen":
-        print("Remíza")
-    elif tah_pocitace == "nůžky":
-        print("Vyhrál jsi")
-    elif tah_pocitace == "papír":
-        print("Prohrál jsi")
-
-elif tah_hrace == "nůžky":
-    if tah_pocitace == "kámen":
-        print("Prohrál jsi")
-    elif tah_pocitace == "nůžky":
-        print("Remíza")
-    elif tah_pocitace == "papír":
-        print("Vyhrál jsi")
-
-elif tah_hrace == "papír":
-    if tah_pocitace == "kámen":
-        print("Vyhrál jsi")
-    elif tah_pocitace == "nůžky":
-        print("Prohrál jsi")
-    elif tah_pocitace == "papír":
-        print("Remíza")
-
-else:
-    print ("Promiň, znám jen tahy kámen, nůžky a papír ")
