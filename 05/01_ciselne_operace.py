3f9534b846b2f49a843f1b82c2d30238cc237002
jiri.one <nemec@jiri.one>
2021-04-11 08:23:45 +0200

    finished mathematical operators

---
+++ 05/01_ciselne_operace.py
@@ -0,0 +1,39 @@
+# nějak mi přišlo, že tohle řešní bude lepší ve třídě, doufám, že to nevadí
+# za předchozí řešení od kolegů se omlouvám, to jsme nekonzultovali :-)
+
+class ZakladniKalkulacka():
+	def __init__(self):
+		self.operatori = {'+': lambda x, y: x + y,
+			 '-': lambda x, y: x - y,
+			 '*': lambda x, y: x * y,
+			 '/': lambda x, y: x / y}
+		self.spusteni_operace()
+		
+	def nacteni_cisel(self):
+		while True:
+			try:
+				input1 = int(input("Zadej první číslo: "))
+				input2 = int(input("Zadej druhé číslo: "))
+			except ValueError:
+				print("Zadej číslo číslem brácho :-)")
+				break
+			return input1, input2
+
+	def nacteni_operace(self):
+		while True:
+			operace = input("Zadej operaci: ")
+			if operace not in self.operatori.keys():
+				print('Musíš zadat "+", "-", "*", "/", nic jiného program nezná.')
+			else:
+				return operace
+
+	def spusteni_operace(self):
+		cislo1, cislo2 = self.nacteni_cisel()
+		operace = self.nacteni_operace()
+		try:
+			print(f"Výsledek je: {self.operatori[operace](cislo1, cislo2)}")
+		except ZeroDivisionError:
+			print("Nulou dělit nelze! Takže výsledek nedostaneš!")
+		
+if __name__ == "__main__":
+	zakladnikalkulacka = ZakladniKalkulacka()
