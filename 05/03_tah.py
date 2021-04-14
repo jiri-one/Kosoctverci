27b3515d219cb0c512d04f2f783c743fda1f2a59
jiri.one <nemec@jiri.one>
2021-04-14 08:30:47 +0200

    some changes

--- 05/03_tah.py
+++ 05/03_tah.py
@@ -1,3 +1,4 @@
+from random import randint
 def tah(hraci_pole, index, symbol):
     """Vrátí herní pole s daným symbolem umístěným na danou pozici:
     `hraci_pole` je herní pole, na které se hraje.
@@ -21,4 +22,13 @@ def tah(hraci_pole, index, symbol):
             print(f"Nový stav hry je: {nove_herni_pole}")
             return nove_herni_pole
 
-print(tah("----x-----o---------", 5, "x"))
+#print(tah("----x-----o---------", 5, "x"))
+nove_hraci_pole = "----x-----o---------"
+for _ in range(100):
+    try:
+        nove_hraci_pole = tah(nove_hraci_pole, randint(0, 19), "x")
+        nove_hraci_pole = tah(nove_hraci_pole, randint(0, 19), "o")
+    except ValueError as error:
+        print(error)
+    if nove_hraci_pole == None:
+        break
--- 05/piskvorky-1D.py
+++ 05/piskvorky-1D.py
@@ -2,18 +2,15 @@ from re import match
 from random import randint
 
 def vyhodnot(hraci_pole):
-    "Vyhodnotí stav hracího pole a vrátí vítěze, remízu nebo řekne, že hra ještě neskončila"
-    if len(hraci_pole) == 20 and isinstance(hraci_pole, str) and match("^[-xo]*$", hraci_pole) is not None:
-        if "xxx" in hraci_pole:
-            return "x"
-        elif "ooo" in hraci_pole:
-            return "o"
-        elif "-" not in hraci_pole:
-            return "!"
-        else:
-            return "-"
+    "Vyhodnotí stav hracího pole a vrátí vítěze, remízu nebo řekne, že hra ještě neskončila" 
+    if "xxx" in hraci_pole:
+        return "x"
+    elif "ooo" in hraci_pole:
+        return "o"
+    elif "-" not in hraci_pole:
+        return "!"
     else:
-        raise ValueError("Špatné hrací pole! Spusť hru znovu!")
+        return "-"
 
 
 def tah(hraci_pole, index, symbol):
@@ -52,45 +49,47 @@ def tah_hrace(pole, symbol):
         except ValueError as error:
                 print(error)
              
-def tah_pocitace(hraci_pole, symbol):
+def tah_pocitace(pole, symbol):
     """Vrátí herní pole se zaznamenaným tahem počítače:
     `pole` je herní pole, na které se hraje.
     `symbol` může být 'x' nebo 'o', podle toho jestli hráč hraje za křížky
     nebo za kolečka.
     """
+    print("Teď táhne počítač.")
     while index_pocitace := randint(0,19):
-        if hraci_pole[index_pocitace] == "-":
-            nove_herni_pole = hraci_pole[:index_pocitace] + symbol + hraci_pole[index_pocitace+1:]
-            print(f"Nový stav hry po tahu počítače je: {nove_herni_pole}")
-            return nove_herni_pole
-
-def piskvorky1d():
-    herni_pole = "-" * 20
-    while hracuv_symbol := input("Jaký symbol chceš mít? x nebo o? "):
-        if hracuv_symbol == "x":
-            symbol_pocitace = "o"
-            break
-        elif hracuv_symbol == "o":
-            symbol_pocitace = "x"
-            break
-        else:
-            print("Dokud nezadáš 'x' nebo nezadáš 'o', tak se budu ptát dokola.")
-    while True:
         try:
-            herni_pole = tah_hrace(herni_pole, hracuv_symbol)
-            if vyhodnot(herni_pole) != "-": break
-            herni_pole = tah_pocitace(herni_pole, symbol_pocitace)
-            if vyhodnot(herni_pole) != "-": break
-        except ValueError as error:
-            print(error)
-            break
-    if vyhodnot(herni_pole) == hracuv_symbol:
-        print("Gratulace hráči, vyhrál jsi!")
-    elif vyhodnot(herni_pole) == symbol_pocitace:
-        print("Promiň hráči, vyhrál počítač. Ale i jemu gratulujeme.")
-    elif vyhodnot(herni_pole) == "!":
-        print("Remíza proti nejlepšímu počítači se taky počítá!")
-    
+            nove_herni_pole = tah(pole, index_pocitace, symbol)
+            return nove_herni_pole
+        except ValueError:
+            pass
+
+
+def piskvorky1d(herni_pole):
+    if len(herni_pole) == 20 and isinstance(herni_pole, str) and match("^[-xo]*$", herni_pole) is not None:
+        while hracuv_symbol := input("Jaký symbol chceš mít? x nebo o? "):
+            if hracuv_symbol == "x":
+                symbol_pocitace = "o"
+                break
+            elif hracuv_symbol == "o":
+                symbol_pocitace = "x"
+                break
+            else:
+                print("Dokud nezadáš 'x' nebo nezadáš 'o', tak se budu ptát dokola.")
+        while True:
+            try:
+                herni_pole = tah_hrace(herni_pole, hracuv_symbol)
+                if vyhodnot(herni_pole) != "-": break
+                herni_pole = tah_pocitace(herni_pole, symbol_pocitace)
+                if vyhodnot(herni_pole) != "-": break                
+            except ValueError as error:
+                print(error)
+        if vyhodnot(herni_pole) == hracuv_symbol:
+            print("Gratulace hráči, vyhrál jsi!")
+        elif vyhodnot(herni_pole) == symbol_pocitace:
+            print("Promiň hráči, vyhrál počítač. Ale i jemu gratulujeme.")
+        elif vyhodnot(herni_pole) == "!":
+            print("Remíza proti nejlepšímu počítači se taky počítá!")
+    else:
+        raise ValueError("Špatné hrací pole! Spusť hru znovu!")  
         
-piskvorky1d()
-#tah_hrace("----x-----o---------", "x")
+piskvorky1d("x-x-x-x-x-x-x-------")
