0316c52114557b3fd5fed7fbec957c2de5a603db
jiri.one <nemec@jiri.one>
2021-04-16 08:56:15 +0200

    finished piskvorky1d with test driven development

--- 06/piskvorky.py
+++ 06/piskvorky.py
@@ -1,4 +1,6 @@
 from util import tah
+from re import match
+from ai import tah_pocitace
 
 def vyhodnot(hraci_pole):
     "Vyhodnotí stav hracího pole a vrátí vítěze, remízu nebo řekne, že hra ještě neskončila" 
@@ -24,3 +26,31 @@ def tah_hrace(pole, symbol):
         except ValueError as error:
                 print(error)
 
+def piskvorky1d():
+    herni_pole = "-" * 20
+    if len(herni_pole) == 20 and isinstance(herni_pole, str) and match("^[-xo]*$", herni_pole) is not None:
+        while hracuv_symbol := input("Jaký symbol chceš mít? x nebo o? "):
+            if hracuv_symbol == "x":
+                symbol_pocitace = "o"
+                break
+            elif hracuv_symbol == "o":
+                symbol_pocitace = "x"
+                break
+            else:
+                print("Dokud nezadáš 'x' nebo nezadáš 'o', tak se budu ptát dokola.")
+        while True:
+            try:
+                herni_pole = tah_hrace(herni_pole, hracuv_symbol)
+                if vyhodnot(herni_pole) != "-": break
+                herni_pole = tah_pocitace(herni_pole, symbol_pocitace)
+                if vyhodnot(herni_pole) != "-": break                
+            except ValueError as error:
+                print(error)
+        if vyhodnot(herni_pole) == hracuv_symbol:
+            print("Gratulace hráči, vyhrál jsi!")
+        elif vyhodnot(herni_pole) == symbol_pocitace:
+            print("Promiň hráči, vyhrál počítač. Ale i jemu gratulujeme.")
+        elif vyhodnot(herni_pole) == "!":
+            print("Remíza proti nejlepšímu počítači se taky počítá!")
+    else:
+        raise ValueError("Špatné hrací pole! Spusť hru znovu!")
