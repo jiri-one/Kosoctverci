3f76eecef630e50be437ccca081b6ad7fb4bc348
jiri.one <nemec@jiri.one>
2021-04-16 13:39:40 +0200

    I am working on new strategy for computer, it is not finished :)

--- 06/ai.py
+++ 06/ai.py
@@ -1,5 +1,23 @@
 from util import tah
-from random import randint
+from random import randint, choice
+
+def strategie_ai(pole, symbol):
+    """Vrátí index navržený dle nějakých lepších strategických pravidel."""
+    # 1. počítač může svým tahem rovnou vyhrát hru
+    if "-" + 2 * symbol in pole: return pole.index("-" + 2 * symbol) # vrátí volný index od např. -xx
+    elif 2 * symbol + "-" in pole: return pole.index(2 * symbol + "-") + 2 # vrátí volný index od např. xx-
+    elif symbol + "-" + symbol in pole: return pole.index(symbol + "-" + symbol) + 1 # vrátí volný index od např. x-x      
+    # 2. může protihráč svým tahem rovnou vyhrát hru (lze zablokovat přidáním znaku zleva, zprava nebo doprostřed)
+    hrace = [z for z in ["x", "o"] if z is not symbol][0] # jakože symbol hráče
+    if "-" + 2 * hrace in pole: return pole.index("-" + 2 * hrace)
+    elif 2 * hrace + "_" in pole: return pole.index(2 * hrace + "-") + 2
+    elif hrace + "-" + hrace in pole: return pole.index(hrace + "-" + hrace)  
+    # 3. můžu si udělat další další svůj symbol vedle už jednoho svého symbolu
+    indexes = [index for index, char in enumerate(pole) if char == symbol]
+    if symbol in pole:
+        return
+    # 4. jinak křížek náhodně kamkoliv, jako v původní verzi
+    
 
 def tah_pocitace(pole, symbol):
     """Vrátí herní pole se zaznamenaným tahem počítače:
@@ -11,8 +29,7 @@ def tah_pocitace(pole, symbol):
     if "-" not in pole:
         raise ValueError("Pole je plně obsazené!")
     while True:
-        index_pocitace = randint(0,19)
-        print(index_pocitace)
+        index_pocitace = strategie_ai(pole, symbol)
         try:
             nove_herni_pole = tah(pole, index_pocitace, symbol)
             return nove_herni_pole
