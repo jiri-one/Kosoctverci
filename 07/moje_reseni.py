e615f45a148b815f135a43e84d24d6c524a533e2
jiri.one <nemec@jiri.one>
2021-04-18 16:21:07 +0200

    finished function serad_od_druheho

--- 07/moje_reseni.py
+++ 07/moje_reseni.py
@@ -20,15 +20,27 @@ def bez_prvniho(jmenny_seznam):
             print(error)
             return error
 
+# tohle mi přijde jednodušší, samozřejmě pokud to bude požadováno, tak mohu udělat i tu zdlouhavou neefektivní verzi
+def serad_od_druheho(seznam):
+    jinak_razeny_seznam = seznam.copy()
+    jinak_razeny_seznam.sort(key=lambda x: x[1])
+    return jinak_razeny_seznam
+    
+
 zvirata = vytvor_seznam_zvirat()
-zvirata_s_kratkym_jmenem = filtruj_kratka_jmena(zvirata)
-zvirata_zacinajici_na_k = filtruj_k(zvirata)
+print(zvirata)
+print(serad_od_druheho(zvirata))
+print(zvirata)
+
+#zvirata = vytvor_seznam_zvirat()
+#zvirata_s_kratkym_jmenem = filtruj_kratka_jmena(zvirata)
+#zvirata_zacinajici_na_k = filtruj_k(zvirata)
 
 
-print(f"Všechna zvířata, i ta s dlouhým jménem, jsou: {zvirata}")
-print(f"Zvířata s krátkým jménem jsou: {zvirata_s_kratkym_jmenem}")
-print(f"Zvířata začínající na 'k' jsou: {zvirata_zacinajici_na_k}")
-print(f"Obsahuje seznam {zvirata} pejska? Odpoved je (anglicky): {obsahuje(zvirata, 'pes')}")
-print(bez_prvniho(zvirata))
-print(bez_prvniho(['pes']))
-print(bez_prvniho([]))
+#print(f"Všechna zvířata, i ta s dlouhým jménem, jsou: {zvirata}")
+#print(f"Zvířata s krátkým jménem jsou: {zvirata_s_kratkym_jmenem}")
+#print(f"Zvířata začínající na 'k' jsou: {zvirata_zacinajici_na_k}")
+#print(f"Obsahuje seznam {zvirata} pejska? Odpoved je (anglicky): {obsahuje(zvirata, 'pes')}")
+#print(bez_prvniho(zvirata))
+#print(bez_prvniho(['pes']))
+#print(bez_prvniho([]))
