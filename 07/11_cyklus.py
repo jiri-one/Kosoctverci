6778ddaee7d6d5ac9ef606ac73a361177de5fc67
jiri.one <nemec@jiri.one>
2021-04-24 08:26:45 +0200

    finished task 15 in Lists and tuples

--- 07/11_cyklus.py
+++ 07/11_cyklus.py
@@ -7,19 +7,19 @@ def pohyb(seznam_souradnic: list, smer: str):
     elif smer == "z": sloupec = sloupec - 1
     elif smer == "v": sloupec = sloupec + 1
     
-    if (radek, sloupec) in seznam_souradnic or radek < 0 or radek > 9 or sloupec < 0 or sloupec > 9:
+    if (radek, sloupec) in seznam_souradnic or radek < 0 or radek > hriste[0]-1 or sloupec < 0 or sloupec > hriste[1]-1:
         raise ValueError('Game over')
     else:
         if (radek, sloupec) in jidlo:
             jidlo.remove((radek, sloupec))
-            jidlo.append((randint(0, 9), randint(0, 9)))
+            jidlo.append((randint(0, hriste[0]-1), randint(0, hriste[1]-1)))
         else:
             seznam_souradnic.pop(0)
         seznam_souradnic.append((radek, sloupec))
         
 def nakresli_mapu(seznam_souradnic: list, jidlo: list):
-    for radek in range(10):
-        for sloupec in range(10):
+    for radek in range(hriste[0]):
+        for sloupec in range(hriste[1]):
             radek_a_sloupec = (radek, sloupec)
             if radek_a_sloupec in jidlo:
                 print("? ", end="")
@@ -32,6 +32,7 @@ def nakresli_mapu(seznam_souradnic: list, jidlo: list):
 
 souradnice = [(0, 0), (0, 1), (0, 2)]
 jidlo = [(2, 3), (4, 5)]
+hriste = (30, 20)
 while smer := input("Kterým směrem pojedeme? "):
     if smer == "konec":
         break
