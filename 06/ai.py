d8c9a92ac8e4bd0978d8490d8ae0a099e8f6a2b5
jiri.one <nemec@jiri.one>
2021-04-24 20:48:01 +0200

    and synced ai with modular non robotic version

--- 06/ai.py
+++ 06/ai.py
@@ -11,37 +11,41 @@ def strategie_ai(pole, symbol):
     hrace = [z for z in ["x", "o"] if z is not symbol][0] # jakože symbol hráče
     if "-" + 2 * hrace in pole: return pole.index("-" + 2 * hrace) # blokuje hráče, pokud má např. -xx
     elif 2 * hrace + "-" in pole: return pole.index(2 * hrace + "-") + 2 # blokuje hráče, pokud má např. xx-
-    elif hrace + "-" + hrace in pole: return pole.index(hrace + "-" + hrace) # blokuje hráče, pokud má např. x-x
+    elif hrace + "-" + hrace in pole: return pole.index(hrace + "-" + hrace) + 1 # blokuje hráče, pokud má např. x-x
     # 3. můžu si udělat další další svůj symbol vedle (nebo ob jeden) už jednoho svého symbolu
     if symbol in pole:
         indexes = [index for index, char in enumerate(pole) if char == symbol] # udělám seznam indexů, kam už táhl
         while indexes:
-            index = choice(indexes) # vyberu náhodně jeden index, kam by chtěl táhnout
             try:
-                # vyberu tu ideální variantu, kdy má na obě strany volná dvě políčka
-                if pole[index-2] == "-" and pole[index+2] == "-" and pole[index-1] == "-" and pole[index+1] == "-":
-                    return choice([index-2, index-1, index+2, index+1])
-                # případně vyberu variantu, kdy má volno 2x vlevo a 1x vpravo
-                elif pole[index-2] == "-" and pole[index-1] == "-" and pole[index+1] == "-":
-                    return choice([index-2, index-1, index+1])
-                # případně vyberu variantu, kdy má volno 2x vpravo a 1x vlevo
-                elif pole[index+2] == "-" and pole[index+1] == "-" and pole[index-1]  == "-":
-                    return choice([index+2, index+1, index-1])
-                # případně vyberu variantu, kdy má volno 2x vpravo
-                elif pole[index+2] == "-" and pole[index+1] == "-":
-                    return choice([index+2, index+1])
-                # případně vyberu variantu, kdy má volno 2x vlevo
-                elif pole[index-2] == "-" and pole[index-1] == "-":
-                    return choice([index-2, index-1])
-                else:
-                    return randint(0,19)
-                indexes.remove(index)
+                index = choice(indexes) # vyberu náhodně jeden index, kam by chtěl táhnout
+                indexes.remove(index) # a rovnou ho odstraním z indexů
             except IndexError:
-                pass
+                break # pokud indexy došly, tak končím smyčku
+            # vyberu tu ideální variantu, kdy má na obě strany volná dvě políčka
+            if "--" + symbol + "--" in pole:
+                start_index = pole.index("--" + symbol + "--")
+                return choice([start_index, start_index+1, start_index+3, start_index+4])
+            # případně vyberu variantu, kdy má volno 2x vlevo a 1x vpravo
+            elif "--" + symbol + "-" in pole:
+                start_index = pole.index("--" + symbol + "-")
+                return choice([start_index, start_index+1, start_index+3])
+            # případně vyberu variantu, kdy má volno 2x vpravo a 1x vlevo
+            elif "-" + symbol + "--" in pole:
+                start_index = pole.index("-" + symbol + "--")
+                return choice([start_index, start_index+2, start_index+3])
+            # případně vyberu variantu, kdy má volno 2x vpravo
+            elif symbol + "--" in pole:
+                start_index = pole.index(symbol + "--")
+                return choice([start_index+1, start_index+2])
+            # případně vyberu variantu, kdy má volno 2x vlevo
+            elif "--" + symbol in pole:
+                start_index = pole.index("--" + symbol)
+                return choice([start_index, start_index+1])
+        else:
+            return randint(0,19)
     # 4. jinak umístím symbol kamkoliv, jako v původní verzi
     else:
         return randint(0,19)
-
 
 def tah_pocitace(pole, symbol):
     """Vrátí herní pole se zaznamenaným tahem počítače:
