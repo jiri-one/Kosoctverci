b50589c48b405cee2b16bb8b23b67148fe0535b5
jiri.one <nemec@jiri.one>
2021-04-24 08:18:49 +0200

    finished task 14 in Lists and tuples

--- 07/09_nakresli_mapu.py
+++ 07/09_nakresli_mapu.py
@@ -1,12 +1,14 @@
-def nakresli_mapu(seznam_souradnic: list):
+def nakresli_mapu(seznam_souradnic: list, jidlo: list):
     for radek in range(10):
         for sloupec in range(10):
             radek_a_sloupec = (radek, sloupec)
-            if radek_a_sloupec in seznam_souradnic:
+            if radek_a_sloupec in jidlo:
+                print("? ", end="")
+            elif radek_a_sloupec in seznam_souradnic:
                 print("X ", end="")
             else:
                 print(". ", end="")
         print()
 
 # test
-nakresli_mapu([(0, 0), (1, 0), (2, 2), (4, 3), (8, 9)])
+nakresli_mapu([(0, 0), (1, 0), (2, 2), (4, 3), (8, 9)], [(2, 3), (4, 5)])
--- 07/11_cyklus.py
+++ 07/11_cyklus.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def pohyb(seznam_souradnic: list, smer: str):
     radek, sloupec = seznam_souradnic[-1]
     if smer == "s": radek = radek - 1
@@ -8,26 +10,33 @@ def pohyb(seznam_souradnic: list, smer: str):
     if (radek, sloupec) in seznam_souradnic or radek < 0 or radek > 9 or sloupec < 0 or sloupec > 9:
         raise ValueError('Game over')
     else:
+        if (radek, sloupec) in jidlo:
+            jidlo.remove((radek, sloupec))
+            jidlo.append((randint(0, 9), randint(0, 9)))
+        else:
+            seznam_souradnic.pop(0)
         seznam_souradnic.append((radek, sloupec))
-        seznam_souradnic.pop(0)
-
-
-def nakresli_mapu(seznam_souradnic: list):
+        
+def nakresli_mapu(seznam_souradnic: list, jidlo: list):
     for radek in range(10):
         for sloupec in range(10):
             radek_a_sloupec = (radek, sloupec)
-            if radek_a_sloupec in seznam_souradnic:
+            if radek_a_sloupec in jidlo:
+                print("? ", end="")
+            elif radek_a_sloupec in seznam_souradnic:
                 print("X ", end="")
             else:
                 print(". ", end="")
         print()
 
+
 souradnice = [(0, 0), (0, 1), (0, 2)]
+jidlo = [(2, 3), (4, 5)]
 while smer := input("Kterým směrem pojedeme? "):
     if smer == "konec":
         break
     else:
         pohyb(souradnice, smer)
-        nakresli_mapu(souradnice)
+        nakresli_mapu(souradnice, jidlo)
         
     
