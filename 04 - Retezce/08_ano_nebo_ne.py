a2e99787b22c57eae55a4ad28436ec34026f96c7
Jiri <jiri@localhost.localdomain>
2021-06-06 10:54:28 +0200

    new bonus task from testing topic

--- 04 - Retezce/08_ano_nebo_ne.py
+++ 04 - Retezce/08_ano_nebo_ne.py
@@ -1,17 +1,17 @@
 def ano_nebo_ne(otazka):
-	"""Vrátí True nebo False podle odpovědi uživatele"""
-	while True:
-		odpoved = input(otazka)
-		odpoved = odpoved.split()
-		if odpoved[0] == 'ano' or odpoved[0] == 'a':
-			return True
-		elif odpoved[0] == 'ne' or odpoved[0] == 'n':
-			return False
+    """Vrátí True nebo False podle odpovědi uživatele"""
+    while True:
+        odpoved = input(otazka)
+        odpoved = odpoved.split()
+        if odpoved[0] == 'ano' or odpoved[0] == 'a':
+            return True
+        elif odpoved[0] == 'ne' or odpoved[0] == 'n':
+            return False
 
-		print('Nerozumím! Odpověz "ano (a)" nebo "ne (n)".')
+        print('Nerozumím! Odpověz "ano (a)" nebo "ne (n)".')
 
 # Příklad použití
 if ano_nebo_ne('Chceš si zahrát hru? '):
-	print('OK! Ale napřed si ji musíš naprogramovat.')
+    print('OK! Ale napřed si ji musíš naprogramovat.')
 else:
-	print('Škoda.')
+    print('Škoda.')
