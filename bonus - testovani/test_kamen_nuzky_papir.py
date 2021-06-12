db328ecd1e18c05c35bfc6e18713741bef701b8e
jiri.one <nemec@jiri.one>
2021-06-12 09:04:39 +0200

    opraveny testy a opraveno issue #3

--- bonus - testovani/test_kamen_nuzky_papir.py
+++ bonus - testovani/test_kamen_nuzky_papir.py
@@ -1,6 +1,13 @@
 from kamen_nuzky_papir import vyhodnot
 
 def test_vyhodnot():
-	assert vyhodnot("kámen", "nůžky") == "kámen"
-	assert vyhodnot("papír", "nůžky") == "nůžky"
-	assert vyhodnot("papír", "kámen") == "papír"
+	assert vyhodnot("kámen", "nůžky") == "a"
+	assert vyhodnot("nůžky", "kámen") == "b"
+	assert vyhodnot("nůžky", "papír") == "a"
+	assert vyhodnot("papír", "nůžky") == "b"
+	assert vyhodnot("papír", "kámen") == "a"
+	assert vyhodnot("kámen", "papír") == "b"
+	assert vyhodnot("kámen", "kámen") == None
+	assert vyhodnot("papír", "papír") == None
+	assert vyhodnot("nůžky", "nůžky") == None
+
